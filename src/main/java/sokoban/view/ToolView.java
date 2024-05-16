24ebc9b7a8fe506095d9de1293b98bf0a37b33b1
Rayan <rayanbenaissa@gmail.com>
2024-05-16 15:17:15 +0200

    refactor:factorisation toolview

--- src/main/java/sokoban/view/ToolView.java
+++ src/main/java/sokoban/view/ToolView.java
@@ -1,10 +1,8 @@
 package sokoban.view;
 
-
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,59 +16,48 @@ import sokoban.viewmodel.ToolViewModel;
 import sokoban.model.*;
 
 public class ToolView extends FlowPane {
-    private final int PADDING = 20;
-    private static final Image player = new Image("player.png");
-    private static final Image box = new Image("box.png");
-    private static final Image ground = new Image("ground.png");
-    private static final Image wall = new Image("wall.png");
-    private static final Image goal = new Image("goal.png");
-    private ToolViewModel toolViewModel;
+    private static final int PADDING = 20;
+    private static final Image[] images = {
+            new Image("ground.png"),
+            new Image("goal.png"),
+            new Image("wall.png"),
+            new Image("player.png"),
+            new Image("box.png")
+    };
 
     public ToolView() {
-
         layoutControls();
-
     }
+
     private void layoutControls() {
-        ImageView playerView = new ImageView(player);
-        ImageView boxView = new ImageView(box);
-        ImageView groundView = new ImageView(ground);
-        ImageView wallView = new ImageView(wall);
-        ImageView goalView = new ImageView(goal);
-
-        addHoverHandler(playerView);
-        addHoverHandler(boxView);
-        addHoverHandler(groundView);
-        addHoverHandler(wallView);
-        addHoverHandler(goalView);
-
-        dragAndRoll(playerView);
-        dragAndRoll(boxView);
-        dragAndRoll(groundView);
-        dragAndRoll(wallView);
-        dragAndRoll(goalView);
-
-        setToolEventHandlers(playerView);
-        setToolEventHandlers(boxView);
-        setToolEventHandlers(groundView);
-        setToolEventHandlers(wallView);
-        setToolEventHandlers(goalView);
-
         setOrientation(Orientation.VERTICAL);
         setAlignment(Pos.CENTER);
         setVgap(20);
         setHgap(20);
-        getChildren().addAll(groundView,goalView,wallView,playerView,boxView);
         setPadding(new Insets(PADDING));
+
+        for (Image image : images) {
+            ImageView imageView = createImageView(image);
+            addHoverHandler(imageView);
+            dragAndRoll(imageView);
+            setToolEventHandlers(imageView);
+            getChildren().add(imageView);
+        }
     }
 
-    // hover a tool
+    private ImageView createImageView(Image image) {
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        imageView.setFitWidth(50); // Taille fixe pour simplifier l'affichage
+        return imageView;
+    }
+
     private void addHoverHandler(ImageView imageView) {
-        imageView.setOnMouseEntered(event -> imageView.setOpacity(0.7)); // Réduire l'opacité de l'image lors du survol
-        imageView.setOnMouseExited(event -> imageView.setOpacity(1.0)); // Rétablir l'opacité normale lorsque la souris quitte l'image
+        imageView.setOnMouseEntered(event -> imageView.setOpacity(0.7));
+        imageView.setOnMouseExited(event -> imageView.setOpacity(1.0));
     }
 
-    private void dragAndRoll(ImageView imageView){
+    private void dragAndRoll(ImageView imageView) {
         imageView.setOnDragDetected(event -> {
             ToolViewModel.setToolSelected(determineToolFromImageView(imageView));
             Dragboard db = imageView.startDragAndDrop(TransferMode.MOVE);
@@ -79,48 +66,46 @@ public class ToolView extends FlowPane {
             db.setContent(content);
             event.consume();
         });
-
     }
 
     private void setToolEventHandlers(ImageView imageView) {
         imageView.setOnMouseClicked(event -> {
             ToolViewModel.setToolSelected(determineToolFromImageView(imageView));
             BoardViewModel.setChanged(true);
-
-            // Réinitialisez le style de tous les outils
-            for (Node child : getChildren()) {
-                child.setEffect(null);
-            }
-
-            // Créez un effet de surbrillance avec une intensité personnalisée
-            DropShadow dropShadow = new DropShadow();
-            dropShadow.setColor(Color.GREY); // Couleur de l'ombre
-            dropShadow.setRadius(10); // Taille du flou
-            dropShadow.setSpread(0.7); // Étalement de l'ombre
-
-            // Appliquez l'effet de surbrillance à l'outil sélectionné
-            imageView.setEffect(dropShadow);
+            applyHighlightEffect(imageView);
         });
     }
 
-
-    // Méthode pour déterminer l'outil correspondant à l'ImageView
-    public static GameElement determineToolFromImageView(ImageView imageView) {
-        Image image = imageView.getImage();
-
-        if (image == player) {
-            return new Player();
-        } else if (image == box) {
-            return new Box();
-        } else if (image == ground) {
-            return new Ground();
-        } else if (image == wall) {
-            return new Wall();
-        } else if (image == goal) {
-            return new Goal();
-        }
-        return null; // Renvoie null si aucune image ne correspond
+    private void applyHighlightEffect(ImageView imageView) {
+        clearHighlightEffects();
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setColor(Color.GREY);
+        dropShadow.setRadius(10);
+        dropShadow.setSpread(0.7);
+        imageView.setEffect(dropShadow);
     }
 
-}
+    private void clearHighlightEffects() {
+        for (javafx.scene.Node child : getChildren()) {
+            if (child instanceof ImageView) {
+                ((ImageView) child).setEffect(null);
+            }
+        }
+    }
 
+    public static GameElement determineToolFromImageView(ImageView imageView) {
+        Image image = imageView.getImage();
+        if (image == images[0]) {
+            return new Ground();
+        } else if (image == images[1]) {
+            return new Goal();
+        } else if (image == images[2]) {
+            return new Wall();
+        } else if (image == images[3]) {
+            return new Player();
+        } else if (image == images[4]) {
+            return new Box();
+        }
+        return null;
+    }
+}
